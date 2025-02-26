053e18af760b91a5be64b1d2d59fe769190ed32d
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-02-26 11:42:44 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Raphael Abano on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
